56af419dbd4c7e8d3beee531619d7f84068ce691
severin-fux <severin.fux@lernende.bbw.ch>
2024-11-04 16:40:17 +0100

    nice jetzt isch natürlich eslint aktiv

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,9 +1,11 @@
-import { NextResponse } from 'next/server';
-import { v4 as uuidv4 } from 'uuid';
-import { shoppingLists } from '../data';
+import {NextResponse} from 'next/server';
+import {v4 as uuidv4} from 'uuid';
+import {shoppingLists} from '../data';
 
 export async function POST(request) {
+
   const code = uuidv4().slice(0, 6);
+  console.log('logging request for debug', request)
   shoppingLists[code] = { items: [], members: [] };
 
   return NextResponse.json({ code });
