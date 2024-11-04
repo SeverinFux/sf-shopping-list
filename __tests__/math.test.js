51e40ce92663406b910aa9c2a6961d82b6bea469
severin-fux <severin.fux@lernende.bbw.ch>
2024-11-04 16:20:48 +0100

    add jest mathe tests

---
+++ __tests__/math.test.js
@@ -0,0 +1,21 @@
+
+//eif mal es paar tests über Mathe
+test('addiert 1 + 2 und gibt 3 zrugg', () => {
+    expect(1 + 2).toBe(3);
+});
+
+test('zieht 5 - 2 ab und gibt 3 zrugg', () => {
+    expect(5 - 2).toBe(3);
+});
+
+test('multipliziert 2 * 3 und gibt 6 zrugg', () => {
+    expect(2 * 3).toBe(6);
+});
+
+test('teilt 6 / 2 und gibt 3 zrugg', () => {
+    expect(6 / 2).toBe(3);
+});
+
+test('modulo 5 % 2 und gibt 1 zrugg', () => {
+    expect(5 % 2).toBe(1);
+});
